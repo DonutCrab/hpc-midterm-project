b9a872f08a58bcd6b5d92af702c5c97cbc825273
DonutCrab <dev@donutcrab.com>
2024-10-23 14:12:25 +0900

    loop approach

--- wine_example.c
+++ wine_example.c
@@ -4,32 +4,18 @@ float wine_concentration_equation(float c1, float dT, float k);
 
 int main()
 {
-    printf("Wine Example:\n");
-
-    // Start Concentration
-    float c1 = 1;
-
-    // Replacement Amount
+    // replacement amount
     float k = 0.1;
 
-    float c2 = wine_concentration_equation(c1, 1, k);
-    printf("day 2 concentration %f\n", c2);
+    float concentration = 1;
+    int days = 10;
 
-    float c3 = wine_concentration_equation(c2, 1, k);
-    printf("day 3 concentration %f\n", c3);
+    for (int i = 0; i < days; i++)
+    {
+        concentration = wine_concentration_equation(concentration, 1, k);
+    }
 
-    float c4 = wine_concentration_equation(c3, 1, k);
-    printf("day 4 concentration %f\n", c4);
-
-    float c5 = wine_concentration_equation(c4, 1, k);
-    printf("day 5 concentration %f\n", c5);
-
-    float c6 = wine_concentration_equation(c5, 1, k);
-    float c7 = wine_concentration_equation(c6, 1, k);
-    float c8 = wine_concentration_equation(c7, 1, k);
-    float c9 = wine_concentration_equation(c8, 1, k);
-    float c10 = wine_concentration_equation(c9, 1, k);
-    printf("day 10 concentration %f\n", c10);
+    printf("Concentration after %d steps: %f\n", days, concentration);
 
     return 0;
 }
